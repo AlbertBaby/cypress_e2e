0c1affef57d8d36d967e8c7b03ea6c3e16dd608b
AlbertBaby <albertbaby266192@gmail.com>
2022-09-23 22:42:55 +0530

    initial commit

---
+++ cypress/Selectors/HomePage.sel.js
@@ -0,0 +1,11 @@
+module.exports={
+    title: 'Peace of mind is just a few clicks away!',
+    imageField: '[ng-model="$ctrl.formData.image"]',
+    usernameField: '[ng-model="$ctrl.formData.username"]',
+    bioField: '[ng-model="$ctrl.formData.bio"]',
+    emailField: '[ng-model="$ctrl.formData.email"]',
+    passwordField: '[ng-model="$ctrl.formData.password"]',
+    submitButton: 'button[type="submit"]',
+    logoutButton: 'button[ng-click="$ctrl.logout()"]',
+    errorMessages: '.error-messages'
+}

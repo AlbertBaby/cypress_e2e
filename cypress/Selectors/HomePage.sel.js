427cc7780caa202a0e7a0f752c88cef8261113ec
AlbertBaby <albertbaby266192@gmail.com>
2023-12-28 12:36:24 +0530

    Homepage actions

--- cypress/Selectors/HomePage.sel.js
+++ cypress/Selectors/HomePage.sel.js
@@ -1,6 +1,9 @@
 module.exports={
-    title: 'Peace of mind is just a few clicks away!',
-    imageField: '[ng-model="$ctrl.formData.image"]',
+    title: 'LinkedIn Job Search: Find US Jobs, Internships, Jobs Near Me',
+    search_job_name:'//button[contains(@class,\'cursor-text mt\')]',
+    search_job_area:'//button[contains(@aria-label,\'Location\')]',
+    search_job_submit:'Search Jobs',
+    imageField: '(//*[contains(text(),\'Jobs\')])[1]',
     usernameField: '[ng-model="$ctrl.formData.username"]',
     bioField: '[ng-model="$ctrl.formData.bio"]',
     emailField: '[ng-model="$ctrl.formData.email"]',
---
+++ readme.md
@@ -0,0 +1,4 @@
+npx cypress run --headless
+npx cypress run --env device=web
+diff eq include
+get variable logging (no general way)
